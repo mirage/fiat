a6213bf9371fe63b1cf3ae2bb0a42912fcac4603
Etienne Millon <etienne.millon@gmail.com>
2019-03-08 11:08:08 +0100

    Use Boringssl C code (#13)
    * Use fe_inv from boringssl
    
    * Use fe_nz
    
    * Use doubling code from boringssl
    
    * Use point addition from boringssl
    
    * Remove unused bindings

--- p256/point.ml
+++ p256/point.ml
@@ -27,8 +27,7 @@ let of_cstruct cs =
   | _ ->
       None
 
-let of_hex h =
-  of_cstruct (Hex.to_cstruct h)
+let of_hex h = of_cstruct (Hex.to_cstruct h)
 
 let of_hex_exn h =
   match of_hex h with
@@ -62,68 +61,25 @@ let to_affine p =
 
 let to_cstruct p =
   match to_affine p with
-  | None -> Cstruct.create 1
+  | None ->
+      Cstruct.create 1
   | Some (x, y) ->
-    let four = Cstruct.create 1 in
-    Cstruct.set_uint8 four 0 4 ;
-    let rev_x = Cstruct_util.rev x
-    and rev_y = Cstruct_util.rev y
-    in
-    Cstruct.concat [ four ; rev_x ; rev_y ]
+      let four = Cstruct.create 1 in
+      Cstruct.set_uint8 four 0 4;
+      let rev_x = Cstruct_util.rev x and rev_y = Cstruct_util.rev y in
+      Cstruct.concat [four; rev_x; rev_y]
 
 let pp fmt p = Cstruct_util.pp_hex_le fmt (Cstruct_util.rev (to_cstruct p))
 
-let double (ctx : Context.t) p =
-  let x_out = Fe.create () in
-  let y_out = Fe.create () in
-  let z_out = Fe.create () in
-  let delta = ctx.delta in
-  let gamma = ctx.gamma in
-  let beta = ctx.beta in
-  let ftmp = ctx.ftmp in
-  let ftmp2 = ctx.ftmp2 in
-  let tmptmp = ctx.tmptmp in
-  let alpha = ctx.alpha in
-  let fourbeta = ctx.fourbeta in
-  (* delta = z^2 *)
-  let z_in = p.f_z in
-  Fe.sqr delta z_in;
-  (* gamma = y^2 *)
-  let y_in = p.f_y in
-  Fe.sqr gamma y_in;
-  (* beta = x*gamma *)
-  let x_in = p.f_x in
-  Fe.mul beta x_in gamma;
-  (* alpha = 3*(x-delta)*(x+delta) *)
-  Fe.sub ftmp x_in delta;
-  Fe.add ftmp2 x_in delta;
-  Fe.add tmptmp ftmp2 ftmp2;
-  Fe.add ftmp2 ftmp2 tmptmp;
-  Fe.mul alpha ftmp ftmp2;
-  (* x' = alpha^2 - 8*beta *)
-  Fe.sqr x_out alpha;
-  Fe.add fourbeta beta beta;
-  Fe.add fourbeta fourbeta fourbeta;
-  Fe.add tmptmp fourbeta fourbeta;
-  Fe.sub x_out x_out tmptmp;
-  (* z' = (y + z)^2 - gamma - delta *)
-  Fe.add delta gamma delta;
-  Fe.add ftmp y_in z_in;
-  Fe.sqr z_out ftmp;
-  Fe.sub z_out z_out delta;
-  (* y' = alpha*(4*beta - x') - 8*gamma^2 *)
-  Fe.sub y_out fourbeta x_out;
-  Fe.add gamma gamma gamma;
-  Fe.sqr gamma gamma;
-  Fe.mul y_out alpha y_out;
-  Fe.add gamma gamma gamma;
-  Fe.sub y_out y_out gamma;
-  {f_x = x_out; f_y = y_out; f_z = z_out}
+external double_c : t -> t -> unit = "fiat_p256_caml_point_double" [@@noalloc]
+
+let double p =
+  let out = {f_x = Fe.create (); f_y = Fe.create (); f_z = Fe.create ()} in
+  double_c out p; out
 
 let%expect_test "double" =
-  let ctx = Context.create () in
   let print_point = Format.printf "%a\n" pp in
-  let test p = print_point @@ double ctx p in
+  let test p = print_point @@ double p in
   let p =
     of_hex_exn
       (`Hex
@@ -143,112 +99,15 @@ let%expect_test "double" =
     {|
     047cf27b188d034f7e8a52380304b51ac3c08969e277f21b35a60b48fc47669978f888aaee24712fc0d6c26539608bcf244582521ac3167dd661fb4862dd878c2e |}]
 
-(**
- add calcuates (x1, y1, z1) + (x2, y2, z2)
+external add_c : t -> t -> t -> unit = "fiat_p256_caml_point_add" [@@noalloc]
 
- The method is taken from:
-   http://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-3.html#addition-add-2007-bl,
- adapted for mixed addition (z2 = 1, or z2 = 0 for the point at infinity).
-
- Coq transcription and correctness proof:
- <https://github.com/mit-plv/fiat-crypto/blob/79f8b5f39ed609339f0233098dee1a3c4e6b3080/src/Curves/Weierstrass/Jacobian.v#L135>
- <https://github.com/mit-plv/fiat-crypto/blob/79f8b5f39ed609339f0233098dee1a3c4e6b3080/src/Curves/Weierstrass/Jacobian.v#L205>
-
- This function includes a branch for checking whether the two input points
- are equal, (while not equal to the point at infinity). This case never
- happens during single point multiplication, so there is no timing leak for
- ECDH or ECDSA signing.
-   *)
-let add (ctx : Context.t) fe_p fe_q =
-  let x_out = Fe.create () in
-  let y_out = Fe.create () in
-  let z_out = Fe.create () in
-  let x1 = fe_p.f_x in
-  let y1 = fe_p.f_y in
-  let z1 = fe_p.f_z in
-  let x2 = fe_q.f_x in
-  let y2 = fe_q.f_y in
-  let z2 = fe_q.f_z in
-  let z1nz = Fe.nz z1 in
-  let z2nz = Fe.nz z2 in
-  (* z1z1 = z1z1 = z1**2 *)
-  let z1z1 = ctx.z1z1 in
-  Fe.sqr z1z1 z1;
-  let u1 = ctx.u1 in
-  let s1 = ctx.s1 in
-  let two_z1z2 = ctx.two_z1z2 in
-  (* z2z2 = z2**2 *)
-  let z2z2 = ctx.z2z2 in
-  Fe.sqr z2z2 z2;
-  (* u1 = x1*z2z2 *)
-  Fe.mul u1 x1 z2z2;
-  (* two_z1z2 = (z1 + z2)**2 - (z1z1 + z2z2) = 2z1z2 *)
-  Fe.add two_z1z2 z1 z2;
-  Fe.sqr two_z1z2 two_z1z2;
-  Fe.sub two_z1z2 two_z1z2 z1z1;
-  Fe.sub two_z1z2 two_z1z2 z2z2;
-  (* s1 = y1 * z2**3 *)
-  Fe.mul s1 z2 z2z2;
-  Fe.mul s1 s1 y1;
-  (* u2 = x2*z1z1 *)
-  let u2 = ctx.u2 in
-  Fe.mul u2 x2 z1z1;
-  (* h = u2 - u1 *)
-  let h = ctx.h in
-  Fe.sub h u2 u1;
-  let xneq = Fe.nz h in
-  (* z_out = two_z1z2 * h *)
-  Fe.mul z_out h two_z1z2;
-  (* z1z1z1 = z1 * z1z1 *)
-  let z1z1z1 = ctx.z1z1z1 in
-  Fe.mul z1z1z1 z1 z1z1;
-  (* s2 = y2 * z1**3 *)
-  let s2 = ctx.s2 in
-  Fe.mul s2 y2 z1z1z1;
-  (* r = (s2 - s1)*2 *)
-  let r = ctx.r in
-  Fe.sub r s2 s1;
-  Fe.add r r r;
-  let yneq = Fe.nz r in
-  if (not xneq) && (not yneq) && z1nz && z2nz then double ctx fe_p
-  else
-    (* I = (2h)**2 *)
-    let i = ctx.i in
-    Fe.add i h h;
-    Fe.sqr i i;
-    (* J = h * I *)
-    let j = ctx.j in
-    Fe.mul j h i;
-    (* V = U1 * I *)
-    let v = ctx.v in
-    Fe.mul v u1 i;
-    (* x_out = r**2 - J - 2V *)
-    Fe.sqr x_out r;
-    Fe.sub x_out x_out j;
-    Fe.sub x_out x_out v;
-    Fe.sub x_out x_out v;
-    (* y_out = r(V-x_out) - 2 * s1 * J *)
-    Fe.sub y_out v x_out;
-    Fe.mul y_out y_out r;
-    let s1j = ctx.s1j in
-    Fe.mul s1j s1 j;
-    Fe.sub y_out y_out s1j;
-    Fe.sub y_out y_out s1j;
-    let x3 = Fe.create () in
-    let y3 = Fe.create () in
-    let z3 = Fe.create () in
-    Fe.cmovznz x_out z1nz x2 x_out;
-    Fe.cmovznz x3 z2nz x1 x_out;
-    Fe.cmovznz y_out z1nz y2 y_out;
-    Fe.cmovznz y3 z2nz y1 y_out;
-    Fe.cmovznz z_out z1nz z2 z_out;
-    Fe.cmovznz z3 z2nz z1 z_out;
-    {f_x = x3; f_y = y3; f_z = z3}
+let add fe_p fe_q =
+  let out = {f_x = Fe.create (); f_y = Fe.create (); f_z = Fe.create ()} in
+  add_c out fe_p fe_q; out
 
 let%expect_test "add" =
-  let ctx = Context.create () in
   let test p q =
-    let r = add ctx p q in
+    let r = add p q in
     Format.printf "%a\n" pp r
   in
   let p =
@@ -298,17 +157,16 @@ let%expect_test "add" =
     "0453020d908b0219328b658b525f26780e3ae12bcd952bb25a93bc0895e1714285b2ba871dd1652c3f467df15c6b70647efbcbbab5cbf7f55e6ff336f843d628a1"]
 
 let%expect_test "double vs add" =
-  let ctx = Context.create () in
   let p =
     of_hex_exn
       (`Hex
         "046B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C2964FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5")
   in
   let print_point = Format.printf "%a\n" pp in
-  print_point @@ add ctx p @@ add ctx p @@ add ctx p p;
+  print_point @@ add p @@ add p @@ add p p;
   [%expect
     {| 04e2534a3532d08fbba02dde659ee62bd0031fe2db785596ef509302446b030852e0f1575a4c633cc719dfee5fda862d764efc96c3f30ee0055c42c23f184ed8c6 |}];
-  print_point @@ double ctx (double ctx p);
+  print_point @@ double @@ double p;
   [%expect
     {| 04e2534a3532d08fbba02dde659ee62bd0031fe2db785596ef509302446b030852e0f1575a4c633cc719dfee5fda862d764efc96c3f30ee0055c42c23f184ed8c6 |}]
 
