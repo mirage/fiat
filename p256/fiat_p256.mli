5cd736c04703e8b36a35515607ccc958feaf24b3
Clément Pascutto <clement@tarides.com>
2019-05-09 16:10:34 +0200

    Add checks details for of_cstruct

--- p256/fiat_p256.mli
+++ p256/fiat_p256.mli
@@ -13,15 +13,16 @@ val pp_point_error : Format.formatter -> point_error -> unit
 (** Pretty printer for point parsing errors *)
 
 val point_of_cs : Cstruct.t -> (point, point_error) result
-(** Convert from cstruct. Refuses the point at infinity.
-    The format is the uncompressed format described in
-    {{http://www.secg.org/sec1-v2.pdf}SEC1}, section 2.3.4, that is to say:
+(** Convert from a cstruct encoding a point in the uncompressed format
+    described in {{http://www.secg.org/sec1-v2.pdf}SEC1}, section 2.3.4.
 
-    - the point at infinity is the single byte "00".
-    - for a point (x, y) not at infinity, the format is:
-      - the byte "04"
-      - x serialized in big endian format, padded to 32 bytes.
-      - y serialized in big endian format, padded to 32 bytes.
+    This performs the following checks:
+    - The header is either `00` or `04`, otherwise returns [Error `Invalid_format].
+    - The point is not at infinity (header `OO`), otherwise returns [Error `At_infinity].
+    - The point coordinate are 32 bytes long, otherwise returns [Error `Invalid_length].
+    - The point coordinates are within 0 and p-1, where p is the curve field order,
+      otherwise returns [Error `Invalid_range].
+    - The point is on the curve, otherwise returns [Error `Not_on_curve].
 *)
 
 val point_of_hex : Hex.t -> (point, point_error) result
@@ -42,9 +43,12 @@ val pp_scalar_error : Format.formatter -> scalar_error -> unit
 (** Pretty printer for scalar parsing errors *)
 
 val scalar_of_cs : Cstruct.t -> (scalar, scalar_error) result
-(** Read data from a cstruct.
-    It should be 32 bytes long, in big endian format. Returns an error when the
-    number is zero, or if it is larger than or equal to the group order. *)
+(** Converts from a 32 bytes big endian cstruct.
+
+    This performs the following checks:
+    - The scalar is 32 bytes long, otherwise returns [Error `Invalid_length].
+    - The scalar is within 1 and n-1, where n is the curve group order, otherwise
+      returns [Error `Invalid_range]. *)
 
 val scalar_of_hex : Hex.t -> (scalar, scalar_error) result
 (** Like [scalar_of_cs] but read from hex data. *)
@@ -54,6 +58,5 @@ val dh : scalar:scalar -> point:point -> Cstruct.t
     scalar multiplication of [point] and [scalar]. *)
 
 val public : scalar -> point
-(** Compute the public key corresponding to a given private key. Internally,
-    this multiplies the generator by the scalar.
+(** Compute the p256 public key corresponding to a given private key.
     Given the invariant on [scalar], the result can't be the point at infinity. *)
