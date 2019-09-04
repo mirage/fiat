37e11c9e7fb7d9d7a283b14d362d6e70c836d428
C For C's Sake <cfcs@users.noreply.github.com>
2019-09-04 12:19:29 +0200

    move to eqaf for compare_be (#46)

--- p256/scalar.ml
+++ p256/scalar.ml
@@ -3,12 +3,14 @@ type t = Scalar of Cstruct.t
 let is_in_range cs =
   let zero = Cstruct.create 32 in
   let n = Hex.to_cstruct Parameters.n in
-  Cstruct_util.compare_be cs zero > 0 && Cstruct_util.compare_be n cs > 0
+  Eqaf_cstruct.compare_be_with_len ~len:32 cs zero > 0
+  && Eqaf_cstruct.compare_be_with_len ~len:32 n cs > 0
 
 let of_cstruct cs =
-  if Cstruct.len cs <> 32 then Error `Invalid_length
-  else if is_in_range cs then Ok (Scalar (Cstruct.rev cs))
-  else Error `Invalid_range
+  match is_in_range cs with
+  | exception Invalid_argument _ -> Error `Invalid_length
+  | true -> Ok (Scalar (Cstruct.rev cs))
+  | false -> Error `Invalid_range
 
 let bit_at (Scalar s) i =
   let byte_num = i / 8 in
